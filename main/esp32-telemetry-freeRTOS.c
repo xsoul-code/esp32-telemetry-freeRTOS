741f75f512d44065ff3608d50f248cc8854e736b
xsoul-code <olek.zachwieja@gmail.com>
2026-02-27 22:43:21 +0100

    Add error handling of dropped packets from full queue.

--- main/esp32-telemetry-freeRTOS.c
+++ main/esp32-telemetry-freeRTOS.c
@@ -27,6 +27,8 @@ void datastream_task(void *pvParameters)
 	
 	//Set the mode in idf.py menuconfig before compiling
 	#ifdef CONFIG_PIR_SIMULATION_MODE
+	uint32_t dropped = 0;
+	//Simulation of PIR Sensor behaviour
 	
 	ESP_LOGI(TAG, "is set to PIR_SIMULATION mode of operation in menuconfig"); //Changing mode using idf.py menuconfig
 	
@@ -34,27 +36,39 @@ void datastream_task(void *pvParameters)
 	{
 		//Timestamp in ms
 		data1.timestamp = esp_timer_get_time() / 1000;
-		//Randomizing a boolean value of PIR for test purposes
+		//Randomizing a boolean value of PIR
 		data1.PIR_sensor = esp_random() % 2;
-		xQueueSend(buffer, ( void * ) &data1, portMAX_DELAY);
+
+		BaseType_t result = xQueueSend(buffer, ( void * ) &data1, 0);
+		if(result == errQUEUE_FULL)
+		{
+			dropped+=1;
+			ESP_LOGW(TAG, " WARNING buffer full %d packet(s) dropped on %d ms reason: errQUEUE_FULL", dropped, data1.timestamp);
+		}
 		vTaskDelay(pdMS_TO_TICKS(1000));
 	}
 	
 	#else
 	//GPIO Mode set in menuconfig -> PIR_Configuration
-
 	ESP_LOGI(TAG, "is set to GPIO mode of operation in menuconfig"); //Changing mode using idf.py menuconfig
+	uint32_t dropped = 0;
 	while(1)
 	{
 		
 		//Timestamp in ms
 		data1.timestamp = esp_timer_get_time() / 1000;
 		//Receiving a sensor data through GPIO
-		data1.PIR_sensor = gpio_get_level(PIRIO); //TODO: Implementation of GPIO handling
-		xQueueSend(buffer, ( void * ) &data1, portMAX_DELAY);
+		data1.PIR_sensor = gpio_get_level(PIRIO);
+
+		BaseType_t result = xQueueSend(buffer, ( void * ) &data1, 0);
+		if(result == errQUEUE_FULL)
+		{
+			dropped+=1;
+			ESP_LOGW(TAG, " error buffer full %d packet(s) dropped on %d ms reason: errQUEUE_FULL", dropped, data1.timestamp);
+		}
 		vTaskDelay(pdMS_TO_TICKS(1000));
 	}
-
+	
 	#endif
 
 }
@@ -64,7 +78,7 @@ void datalogger_task(void *pvParameters)
 	datasensor_t data2;
 	while(1)
 	{
-		if(xQueueReceive(buffer, ( void * ) &data2, portMAX_DELAY))
+		if(xQueueReceive(buffer, ( void * ) &data2, pdMS_TO_TICKS(500)))
 		{
 			ESP_LOGI(TAG, "captured on: %d ms with state %d",data2.timestamp, data2.PIR_sensor);
 		}
@@ -74,8 +88,7 @@ void datalogger_task(void *pvParameters)
 
 void app_main(void)
 {
-	//GPIO setup: interrupt HIGH, PULL DOWN
-	
+	//GPIO setup
 	gpio_config_t pir_config ={
 		.pin_bit_mask = (1ULL << PIRIO),
 		.mode = GPIO_MODE_INPUT,
@@ -84,7 +97,7 @@ void app_main(void)
 		.intr_type = GPIO_INTR_DISABLE
 	};
 	gpio_config(&pir_config);
-	buffer = xQueueCreate(1, sizeof(datasensor_t));
+	buffer = xQueueCreate(2, sizeof(datasensor_t));
 
 	xTaskCreate(datastream_task, "data_stream", 2048, NULL, 5, NULL);
 	xTaskCreate(datalogger_task, "data_logger", 2048, NULL, 5, NULL);
